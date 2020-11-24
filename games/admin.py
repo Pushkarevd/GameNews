5027ac0535efd06090f73155c7a15db499447ceb
Srezor <pushkarevd.89@gmail.com>
2020-11-24 18:36:52 +0300

    Fix admin page fields, and text on login page

--- games/admin.py
+++ games/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from .models import StopGameHeadline, IgnHeadline
 
@@ -10,6 +11,12 @@ class StopGameAdmin(admin.ModelAdmin):
 class IgnAdmin(admin.ModelAdmin):
     fields = ('id', 'title', 'desc')
 
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username', "email", 'user_permissions', 'is_active', 'last_login', 'groups', 'is_staff')
+    readonly_fields = ('username', "email", 'is_active', 'last_login')
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(StopGameHeadline, StopGameAdmin)
 admin.site.register(IgnHeadline, IgnAdmin)
--- games/models.py
+++ games/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class StopGameHeadline(models.Model):
-    desc = models.CharField(max_length=256)
+    desc = models.CharField(max_length=256, )
     img_url = models.URLField(max_length=256)
     news_url = models.URLField(max_length=256, primary_key=True)
     id = models.IntegerField(default=None)
